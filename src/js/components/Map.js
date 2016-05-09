05883ec385b4f2e3289f50064cf164daab9acf40
John J Czaplewski <jczaplew@gmail.com>
2016-05-09 11:31:30 -0500

    Add hack for south africa

--- src/js/components/Map.js
+++ src/js/components/Map.js
@@ -271,9 +271,9 @@ var Map = React.createClass({
           });
         }
 
-        // Hack to get articles for Australia and UK medium in the absence of Macrostrat matches
+        // Hack to get articles for Australia, UK medium, and South Africa in the absence of Macrostrat matches
         bestFit.forEach(unit => {
-          if ((unit.source_id === 5 || unit.source_id === 23) && unit.strat_name.length) {
+          if ((unit.source_id === 5 || unit.source_id === 23 || unit.source_id === 41) && unit.strat_name.length) {
             var name = (unit.strat_name.indexOf(' of ') > -1) ? unit.strat_name.split(' of ')[0] : unit.strat_name;
             this.getArticles([name]);
           }
